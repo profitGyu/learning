8f54cf66cf238d66eb6b364ba6edbd99cb88a90b
profitgyu <profitgyu@profitgyuui-MacBookPro.local>
2025-09-10 06:59:50 +0900

    refactor: 영상 타이머 싱크 조정

--- src/data/songs/artists/japanese-artist/rashisa.ts
+++ src/data/songs/artists/japanese-artist/rashisa.ts
@@ -9,167 +9,79 @@ export const rashisa: YouTubeSong = {
   lyrics: [
     { 
       startTime: 16, 
-      endTime: 20,
-      japanese: "誇るよ全部 僕が僕であるための要素を", 
-      romaji: "hokoru yo zenbu boku ga boku de aru tame no youso wo", 
-      korean: "자랑스러워 전부 내가 나로 있기 위한 요소를",
-      koreanPronunciation: "호코루 요 젠부 보쿠가 보쿠데 아루 타메노 요소오"
-    },
-    { 
-      startTime: 20, 
-      endTime: 24,
-      japanese: "好きだよ全部 君という僕の黒い部分も", 
-      romaji: "suki da yo zenbu kimi to iu boku no kuroi bubun mo", 
-      korean: "좋아해 전부 너라는 나의 검은 부분도",
-      koreanPronunciation: "스키다 요 젠부 키미토 이우 보쿠노 쿠로이 부분모"
+      endTime: 27,
+      japanese: "誇るよ全部 僕が僕であるための要素を 好きだよ全部 君という僕の黒い部分も", 
+      romaji: "hokoru yo zenbu boku ga boku de aru tame no youso wo suki da yo zenbu kimi to iu boku no kuroi bubun mo", 
+      korean: "자랑스러워 전부 내가 나로 있기 위한 요소를 좋아해 전부 너라는 나의 검은 부분도",
+      koreanPronunciation: "호코루 요 젠부 보쿠가 보쿠데 아루 타메노 요소오 스키다 요 젠부 키미토 이우 보쿠노 쿠로이 부분모"
     },
     { 
       startTime: 28, 
-      endTime: 32,
-      japanese: "恵まれなかった才能も 丈夫じゃない性格も", 
-      romaji: "megumarenakatta sainou mo joubu ja nai seikaku mo", 
-      korean: "타고나지 못한 재능도 굳세지 못한 성격도",
-      koreanPronunciation: "메구마레나카타 사이노우모 조우부쟈나이 세이카쿠모" 
-    },
-    { 
-      startTime: 32, 
-      endTime: 36,
-      japanese: "だけど大それた夢をちゃんと描く強かさも", 
-      romaji: "dakedo oosoreta yume wo chanto egaku shitakasa mo", 
-      korean: "그래도 터무니없는 꿈을 제대로 그려내는 강인함도",
-      koreanPronunciation: "다케도 오오소레타 유메오 찬토 에가쿠 시타카사모"
+      endTime: 38,
+      japanese: "恵まれなかった才能も 丈夫じゃない性格ㅌㅈも だけど大それた夢をちゃんと描く強かさも", 
+      romaji: "megumarenakatta sainou mo joubu ja nai seikaku mo dakedo oosoreta yume wo chanto egaku shitakasa mo", 
+      korean: "타고나지 못한 재능도 굳세지 못한 성격도 그래도 터무니없는 꿈을 제대로 그려내는 강인함도",
+      koreanPronunciation: "메구마레나카타 사이노우모 조우부쟈나이 세이카쿠모 다케도 오오소레타 유메오 찬토 에가쿠 시타카사모"
     },
     { 
       startTime: 39, 
-      endTime: 43,
-      japanese: "焦るよいつも 足音の群衆が僕の努力を", 
-      romaji: "aseru yo itsumo ashioto no gunshuu ga boku no doryoku wo", 
-      korean: "초조해 언제나 발소리의 군중이 나의 노력을",
-      koreanPronunciation: "아세루 요 이츠모 아시오토노 군슈우가 보쿠노 도료쿠오"
-    },
-    { 
-      startTime: 43, 
-      endTime: 47,
-      japanese: "引き裂いて何度 君という闇の世話になったろう", 
-      romaji: "hikisaite nando kimi to iu yami no sewa ni natta rou", 
-      korean: "찢어 놓아 몇 번이나 너라는 어둠의 신세를 졌었지",
-      koreanPronunciation: "히키사이테 난도 키미토 이우 야미노 세와니 낫타로우"
+      endTime: 50,
+      japanese: "焦るよいつも 足音の群衆が僕の努力を 引き裂いて何度 君という闇の世話になったろう", 
+      romaji: "aseru yo itsumo ashioto no gunshuu ga boku no doryoku wo hikisaite nando kimi to iu yami no sewa ni natta rou", 
+      korean: "초조해 언제나 발소리의 군중이 나의 노력을 찢어 놓아 몇 번이나 너라는 어둠의 신세를 졌었지",
+      koreanPronunciation: "아세루 요 이츠모 아시오토노 군슈우가 보쿠노 도료쿠오 히키사이테 난도 키미토 이우 야미노 세와니 낫타로우"
     },
     { 
       startTime: 51, 
-      endTime: 55,
-      japanese: "オンリーワンでもいいと 無理やりつけたアイマスクの", 
-      romaji: "onrii wan demo ii to muriyari tsuketa ai masuku no", 
-      korean: "온리 원이라도 괜찮다고 억지로 쓴 아이 마스크",
-      koreanPronunciation: "온리 완데모 이이토 무리야리 츠케타 아이 마스쿠노"
-    },
-    { 
-      startTime: 55, 
-      endTime: 59,
-      japanese: "奥で一睡もしやしない自分も見飽きたよ", 
-      romaji: "oku de issui mo shiya shinai jibun mo miakita yo", 
-      korean: "속에서 한숨도 못 자는 자신도 질려버렸어",
-      koreanPronunciation: "오쿠데 잇스이모 시야 시나이 지분모 미아키타 요"
+      endTime: 62,
+      japanese: "オンリーワンでもいいと 無理やりつけたアイマスクの 奥で一睡もしやしない自分も見飽きたよ", 
+      romaji: "onrii wan demo ii to muriyari tsuketa ai masuku no oku de issui mo shiya shinai jibun mo miakita yo", 
+      korean: "온리 원이라도 괜찮다고 억지로 쓴 아이 마스크 속에서 한숨도 못 자는 자신도 질려버렸어",
+      koreanPronunciation: "온리 완데모 이이토 무리야리 츠케타 아이 마스쿠노 오쿠데 잇스이모 시야 시나이 지분모 미아키타 요"
     },
     { 
       startTime: 63, 
-      endTime: 67,
-      japanese: "現実的、客観的に見れば 絶望的。", 
-      romaji: "genjitsuteki, kyakkanteki ni mireba zetsubouteki.", 
-      korean: "현실적, 객관적으로 보면 절망적.",
-      koreanPronunciation: "겐지츠테키, 캭간테키니 미레바 제츠보우테키"
-    },
-    { 
-      startTime: 67, 
-      endTime: 71,
-      japanese: "絶対的1位はきっと取れないな", 
-      romaji: "zettaiteki ichii wa kitto torenai na", 
-      korean: "절대적 1등은 분명 못 할 거야",
-      koreanPronunciation: "젯타이테키 이치이와 키토 토레나이 나"
+      endTime: 70,
+      japanese: "現実的、客観的に見れば 絶望的。 絶対的1位はきっと取れないな", 
+      romaji: "genjitsuteki, kyakkanteki ni mireba zetsubouteki. zettaiteki ichii wa kitto torenai na", 
+      korean: "현실적, 객관적으로 보면 절망적. 절대적 1등은 분명 못 할 거야",
+      koreanPronunciation: "겐지츠테키, 캭간테키니 미레바 제츠보우테키 젯타이테키 이치이와 키토 토레나이 나"
     },
     { 
       startTime: 71, 
-      endTime: 75,
-      japanese: "分かってる 分かっちゃいるんだけど", 
-      romaji: "wakatteru wakaccha iru n da kedo", 
-      korean: "알고 있어, 알고는 있지만",
-      koreanPronunciation: "와카테루 와카챠 이룬다케도"
-    },
-    { 
-      startTime: 75, 
-      endTime: 80,
-      japanese: "圧倒的、直感的に僕は 納得出来出来ちゃいない", 
-      romaji: "attouteki, chokanteki ni boku wa nattoku deki deki cha inai", 
-      korean: "압도적, 직감적으로 나는 납득할 수가 없어",
-      koreanPronunciation: "아토우테키, 초칸테키니 보쿠와 난토쿠 데키 데키 챠 이나이"
-    },
-    { 
-      startTime: 80, 
-      endTime: 86,
-      japanese: "ああ なんでこんなにも面倒で 不適合な長所を宿してしまったんだろう", 
-      romaji: "aa nande konna ni mo mendou de futekigou na chousho wo yadoshite shimatta n darou", 
-      korean: "아 왜 이렇게 귀찮고 부적합한 장점을 품어버린 걸까",
-      koreanPronunciation: "아 난데 코난니모 멘도우데 후테키고우나 초우쇼오 야도시테 시마타다로우"
+      endTime: 92,
+      japanese: "分かってる 分かっちゃいるんだけど 圧倒的、直感的に僕は 納得出来出来ちゃいない ああ なんでこんなにも面倒で 不適合な長所を宿してしまったんだろう", 
+      romaji: "wakatteru wakaccha iru n da kedo attouteki, chokanteki ni boku wa nattoku deki deki cha inai aa nande konna ni mo mendou de futekigou na chousho wo yadoshite shimatta n darou", 
+      korean: "알고 있어, 알고는 있지만 압도적, 직감적으로 나는 납득할 수가 없어 아 왜 이렇게 귀찮고 부적합한 장점을 품어버린 걸까",
+      koreanPronunciation: "와카테루 와카챠 이룬다케도 아토우테키, 초칸테키니 보쿠와 난토쿠 데키 데키 챠 이나이 아 난데 코난니모 멘도우데 후테키고우나 초우쇼오 야도시테 시마타다로우"
     },
     { 
       startTime: 93, 
-      endTime: 97,
-      japanese: "らしさ そんなものを抱えては", 
-      romaji: "rashisa sonna mono wo kakaete wa", 
-      korean: "나다움 그런 걸 안고서는",
-      koreanPronunciation: "라시사 손나 모노오 카카에테 와"
-    },
-    { 
-      startTime: 97, 
-      endTime: 101,
-      japanese: "僕らは泣いてた 笑ってた 競い合ったまま", 
-      romaji: "bokura wa naiteta waratteta kisoiatta mama", 
-      korean: "우리는 울고 웃었어 서로 경쟁하면서도",
-      koreanPronunciation: "보쿠라와 나이테타 와랏테타 키소잇타 마마"
+      endTime: 103,
+      japanese: "らしさ そんなものを抱えては 僕らは泣いてた 笑ってた 競い合ったまま", 
+      romaji: "rashisa sonna mono wo kakaete wa bokura wa naiteta waratteta kisoiatta mama", 
+      korean: "나다움 그런 걸 안고서는 우리는 울고 웃었어 서로 경쟁하면서도",
+      koreanPronunciation: "라시사 손나 모노오 카카에테 와 보쿠라와 나이테타 와랏테타 키소잇타 마마"
     },
     { 
       startTime: 104, 
-      endTime: 108,
-      japanese: "大好きな事に全て捧げては", 
-      romaji: "daisuki na koto ni subete sasagete wa", 
-      korean: "정말 좋아하는 것에 모든 걸 바치고는",
-      koreanPronunciation: "다이스키나 코토니 스베테 사사게테 와"
-    },
-    { 
-      startTime: 108, 
-      endTime: 114,
-      japanese: "何度も泣くんだ 笑うんだ メチャクチャなペースで", 
-      romaji: "nando mo naku n da warau n da mechakucha na peesu de", 
-      korean: "몇 번이나 울고 웃는다 엉망진창인 페이스로",
-      koreanPronunciation: "난도모 나쿤다 와라운다 메차쿠차나 페-스데"
+      endTime: 117,
+      japanese: "大好きな事に全て捧げては 何度も泣くんだ 笑うんだ メチャクチャなペースで", 
+      romaji: "daisuki na koto ni subete sasagete wa nando mo naku n da warau n da mechakucha na peesu de", 
+      korean: "정말 좋아하는 것에 모든 걸 바치고는 몇 번이나 울고 웃는다 엉망진창인 페이스로",
+      koreanPronunciation: "다이스키나 코토니 스베테 사사게테 와 난도모 나쿤다 와라운다 메차쿠차나 페-스데"
     },
     { 
       startTime: 118, 
-      endTime: 120,
-      japanese: "これしかないから", 
-      romaji: "kore shika nai kara", 
-      korean: "이것밖에 없으니까",
-      koreanPronunciation: "코레시카 나이카라"
-    },
-    { 
-      startTime: 121, 
-      endTime: 123,
-      japanese: "しがみついてたかったんだ", 
-      romaji: "shigamitsuiteta katta n da", 
-      korean: "매달리고 싶었던 거야",
-      koreanPronunciation: "시가미츠이테타 카타다"
-    },
-    { 
-      startTime: 124, 
-      endTime: 126,
-      japanese: "居場所が欲しかったんだ", 
-      romaji: "ibasho ga hoshikatta n da", 
-      korean: "자리가 필요했던 거야",
-      koreanPronunciation: "이바쇼가 호시카타다"
+      endTime: 127,
+      japanese: "これしかないから しがみついてたかったんだ 居場所が欲しかったんだ", 
+      romaji: "kore shika nai kara shigamitsuiteta katta n da ibasho ga hoshikatta n da", 
+      korean: "이것밖에 없으니까 매달리고 싶었던 거야 자리가 필요했던 거야",
+      koreanPronunciation: "코레시카 나이카라 시가미츠이테타 카타다 이바쇼가 호시카타다"
     },
     { 
       startTime: 128, 
-      endTime: 132,
+      endTime: 133,
       japanese: "何度勝っても それはそれで未来は苦しいもの", 
       romaji: "nando kattemo sore wa sore de mirai wa kurushii mono", 
       korean: "몇 번을 이겨도 그건 그거대로 미래는 괴로운 것",
@@ -177,187 +89,75 @@ export const rashisa: YouTubeSong = {
     },
     { 
       startTime: 134, 
-      endTime: 138,
-      japanese: "ああ まじでなんなの？ 負けて負けて負けまくった時ほど", 
-      romaji: "aa maji de nan na no? makete makete makemakutta toki hodo", 
-      korean: "아 정말 뭐야? 지고 또 지고 지기만 할 때일수록",
-      koreanPronunciation: "아 마지데 난나노? 마케테 마케테 마케마쿠타 토키 호도"
-    },
-    { 
-      startTime: 140, 
-      endTime: 146,
-      japanese: "思い出話が僕の歩みを遅めてしまうよ 過去の僕と君からの最低のプレゼント", 
-      romaji: "omoide banashi ga boku no ayumi wo osomete shimau yo kako no boku to kimi kara no saitei no purezento", 
-      korean: "추억 이야기가 나의 발걸음을 늦추어 버려 과거의 나와 너로부터의 최악의 선물",
-      koreanPronunciation: "오모이데 바나시가 보쿠노 아유미오 오소메테 시마우 요 카코노 보쿠토 키미카라노 사이테이노 프레젠토"
+      endTime: 151,
+      japanese: "ああ まじでなんなの？ 負けて負けて負けまくった時ほど 思い出話が僕の歩みを遅めてしまうよ 過去の僕と君からの最低のプレゼント", 
+      romaji: "aa maji de nan na no? makete makete makemakutta toki hodo omoide banashi ga boku no ayumi wo osomete shimau yo kako no boku to kimi kara no saitei no purezento", 
+      korean: "아 정말 뭐야? 지고 또 지고 지기만 할 때일수록 추억 이야기가 나의 발걸음을 늦추어 버려 과거의 나와 너로부터의 최악의 선물",
+      koreanPronunciation: "아 마지데 난나노? 마케테 마케테 마케마쿠타 토키 호도 오모이데 바나시가 보쿠노 아유미오 오소메테 시마우 요 카코노 보쿠토 키미카라노 사이테이노 프레젠토"
     },
     { 
       startTime: 152, 
-      endTime: 156,
-      japanese: "打算的で消極的な面は 僕を守る絆創膏", 
-      romaji: "dasanteki de shoukyokuteki na men wa boku wo mamoru bansoukou", 
-      korean: "타산적이고 소극적인 면은 나를 지키는 반창고",
-      koreanPronunciation: "다산테키데 쇼우쿄쿠테키나 멘와 보쿠오 마모루 반소우코우"
-    },
-    { 
-      startTime: 156, 
-      endTime: 160,
-      japanese: "怖くてきっと剥げないな", 
-      romaji: "kowakute kitto hagenai na", 
-      korean: "무서워서 분명 떼지 못할 거야",
-      koreanPronunciation: "코와쿠테 키토 하게나이 나"
+      endTime: 159,
+      japanese: "打算的で消極的な面は 僕を守る絆創膏 怖くてきっと剥げないな", 
+      romaji: "dasanteki de shoukyokuteki na men wa boku wo mamoru bansoukou kowakute kitto hagenai na", 
+      korean: "타산적이고 소극적인 면은 나를 지키는 반창고 무서워서 분명 떼지 못할 거야",
+      koreanPronunciation: "다산테키데 쇼우쿄쿠테키나 멘와 보쿠오 마모루 반소우코우 코와쿠테 키토 하게나이 나"
     },
     { 
       startTime: 160, 
-      endTime: 164,
-      japanese: "分かってる 分かっちゃいるんだけど", 
-      romaji: "wakatteru wakaccha iru n da kedo", 
-      korean: "알고 있어, 알고는 있지만",
-      koreanPronunciation: "와카테루 와카챠 이룬다케도"
-    },
-    { 
-      startTime: 164, 
-      endTime: 169,
-      japanese: "プライドは新品のままで白くふやけ痒がっている", 
-      romaji: "puraido wa shinpin no mama de shiroku fuyake kayagatte iru", 
-      korean: "프라이드는 새것인 채로 하얗게 불어서 가려워하고 있어",
-      koreanPronunciation: "프라이도와 신핌노 마마데 시로쿠 후야케 카야갓테 이루"
-    },
-    { 
-      startTime: 169, 
-      endTime: 175,
-      japanese: "ああ どちらも僕で君だから どちらも本心だからたちが悪いよな", 
-      romaji: "aa dochira mo boku de kimi da kara dochira mo honshin da kara tachi ga warui yo na", 
-      korean: "아 어느 쪽이든 나이고 너니까 둘 다 진심이니까 골치 아파",
-      koreanPronunciation: "아 도치라모 보쿠데 키미다카라 도치라모 혼신다카라 타치가 와루이 요나"
+      endTime: 198,
+      japanese: "分かってる 分かっちゃいるんだけど プライドは新品のままで白くふやけ痒がっている ああ どちらも僕で君だから どちらも本心だからたちが悪いよな", 
+      romaji: "wakatteru wakaccha iru n da kedo puraido wa shinpin no mama de shiroku fuyake kayagatte iru aa dochira mo boku de kimi da kara dochira mo honshin da kara tachi ga warui yo na", 
+      korean: "알고 있어, 알고는 있지만 프라이드는 새것인 채로 하얗게 불어서 가려워하고 있어 아 어느 쪽이든 나이고 너니까 둘 다 진심이니까 골치 아파",
+      koreanPronunciation: "와카테루 와카챠 이룬다케도 프라이도와 신핌노 마마데 시로쿠 후야케 카야갓테 이루 아 도치라모 보쿠데 키미다카라 도치라모 혼신다카라 타치가 와루이 요나"
     },
     { 
       startTime: 199, 
-      endTime: 203,
-      japanese: "始めたのが遅いから", 
-      romaji: "hajimeta no ga osoi kara", 
-      korean: "시작이 늦었으니까"
+      endTime: 210,
+      japanese: "始めたのが遅いから 世界はあまりにも広いから 天才はレベルが違うから てかお前が楽しけりゃいいじゃん", 
+      romaji: "hajimeta no ga osoi kara sekai wa amari ni mo hiroi kara tensai wa reberu ga chigau kara teka omae ga tanoshikerya ii jan", 
+      korean: "시작이 늦었으니까 세상은 너무나도 넓으니까 천재는 레벨이 다르니까 그래도 네가 즐거우면 됐잖아",
+      koreanPronunciation: "하지메타노가 오소이카라 세카이와 아마리니모 히로이카라 텐사이와 레베루가 치가우카라 테카 오마에가 타노시케랴 이이잔"
     },
     { 
-      startTime: 203, 
-      endTime: 207,
-      japanese: "世界はあまりにも広いから", 
-      romaji: "sekai wa amari ni mo hiroi kara", 
-      korean: "세상은 너무나도 넓으니까"
-    },
-    { 
-      startTime: 207, 
-      endTime: 211,
-      japanese: "天才はレベルが違うから", 
-      romaji: "tensai wa reberu ga chigau kara", 
-      korean: "천재는 레벨이 다르니까"
-    },
-    { 
-      startTime: 211, 
-      endTime: 215,
-      japanese: "てかお前が楽しけりゃいいじゃん", 
-      romaji: "teka omae ga tanoshikerya ii jan", 
-      korean: "그래도 네가 즐거우면 됐잖아"
-    },
-    { 
-      startTime: 215, 
-      endTime: 219,
-      japanese: "ああ うるさいな それでもなんか君に負けてしまう日もあった", 
-      romaji: "aa urusai na sore demo nanka kimi ni makete shimau hi mo atta", 
-      korean: "아 시끄러워 그래도 왠지 너에게 져버리는 날도 있었지"
-    },
-    { 
-      startTime: 219, 
-      endTime: 223,
-      japanese: "まあ そりゃそっか ブレない芯や思想なんて僕らしくはないや", 
-      romaji: "maa sorya sokka burenai shin ya shisou nante boku rashiku wa nai ya", 
-      korean: "뭐, 그건 그래 흔들리지 않는 심지나 사상 따위는 나답지 않아"
-    },
-    { 
-      startTime: 223, 
-      endTime: 227,
-      japanese: "でも今の僕はもう限界だ 君の言いなりになってたまるか", 
-      romaji: "demo ima no boku wa mou genkai da kimi no iinari ni natte tamaru ka", 
-      korean: "하지만 지금의 나는 이미 한계야 네 말대로 하는 건 견딜 수 없어"
-    },
-    { 
-      startTime: 227, 
-      endTime: 233,
-      japanese: "僕はやっぱ誰にも負けたくないんだ そんな熱よどうか 消えてなくなるな！", 
-      romaji: "boku wa yappa dare ni mo maketakunai n da sonna netsu yo douka kiete nakunaru na!", 
-      korean: "나는 역시 누구에게도 지고 싶지 않아 그런 열정이여 제발 사라지지 마!",
-      koreanPronunciation: "보쿠와 야파 다레니모 마케타쿠나인다 솜나 네츠요 도우카 키에테 나쿠나루나!"
+      startTime: 210, 
+      endTime: 240,
+      japanese: "ああ うるさいな それでもなんか君に負けてしまう日もあった まあ そりゃそっか ブレない芯や思想なんて僕らしくはないや でも今の僕はもう限界だ 君の言いなりになってたまるか 僕はやっぱ誰にも負けたくないんだ そんな熱よどうか 消えてなくなるな！", 
+      romaji: "aa urusai na sore demo nanka kimi ni makete shimau hi mo atta maa sorya sokka burenai shin ya shisou nante boku rashiku wa nai ya demo ima no boku wa mou genkai da kimi no iinari ni natte tamaru ka boku wa yappa dare ni mo maketakunai n da sonna netsu yo douka kiete nakunaru na!", 
+      korean: "아 시끄러워 그래도 왠지 너에게 져버리는 날도 있었지 뭐, 그건 그래 흔들리지 않는 심지나 사상 따위는 나답지 않아 하지만 지금의 나는 이미 한계야 네 말대로 하는 건 견딜 수 없어 나는 역시 누구에게도 지고 싶지 않아 그런 열정이여 제발 사라지지 마!",
+      koreanPronunciation: "아 우루사이나 소레데모 난카 키미니 마케테 시마우 히모 앗타 마 소랴 솟카 부레나이 신야 시소우난테 보쿠라시쿠와 나이야 데모 이마노 보쿠와 모우 겐카이다 키미노 이이나리니 낫테 타마루카 보쿠와 야파 다레니모 마케타쿠나인다 솜나 네츠요 도우카 키에테 나쿠나루나!"
     },
     { 
       startTime: 241, 
-      endTime: 245,
-      japanese: "らしさ そんなものを抱えては", 
-      romaji: "rashisa sonna mono wo kakaete wa", 
-      korean: "나다움 그런 걸 안고서는"
-    },
-    { 
-      startTime: 245, 
-      endTime: 249,
-      japanese: "僕らは泣いてた 笑ってた 競い合ったまま", 
-      romaji: "bokura wa naiteta waratteta kisoiatta mama", 
-      korean: "우리는 울고 웃었어 서로 경쟁하면서도"
+      endTime: 252,
+      japanese: "らしさ そんなものを抱えては 僕らは泣いてた 笑ってた 競い合ったまま", 
+      romaji: "rashisa sonna mono wo kakaete wa bokura wa naiteta waratteta kisoiatta mama", 
+      korean: "나다움 그런 걸 안고서는 우리는 울고 웃었어 서로 경쟁하면서도",
+      koreanPronunciation: "라시사 손나 모노오 카카에테 와 보쿠라와 나이테타 와랏테타 키소잇타 마마"
     },
     { 
       startTime: 253, 
-      endTime: 257,
-      japanese: "大好きな事に全て捧げては", 
-      romaji: "daisuki na koto ni subete sasagete wa", 
-      korean: "정말 좋아하는 것에 모든 걸 바치고는"
-    },
-    { 
-      startTime: 257, 
-      endTime: 261,
-      japanese: "まだまだ泣くんだ 笑うんだ", 
-      romaji: "mada mada naku n da warau n da", 
-      korean: "아직도 울고 웃는 거야"
-    },
-    { 
-      startTime: 262, 
-      endTime: 266,
-      japanese: "位置についたなら さあ、本気で勝負だ", 
-      romaji: "ichi ni tsuita nara saa, honki de shoubu da", 
-      korean: "제자리에 섰다면 자, 진심으로 승부다"
+      endTime: 265,
+      japanese: "大好きな事に全て捧げては まだまだ泣くんだ 笑うんだ 位置についたなら さあ、本気で勝負だ", 
+      romaji: "daisuki na koto ni subete sasagete wa mada mada naku n da warau n da ichi ni tsuita nara saa, honki de shoubu da", 
+      korean: "정말 좋아하는 것에 모든 걸 바치고는 아직도 울고 웃는 거야 제자리에 섰다면 자, 진심으로 승부다",
+      koreanPronunciation: "다이스키나 코토니 스베테 사사게테 와 마다 마다 나쿤다 와라운다 이치니 츠이타나라 사, 혼키데 쇼우부다"
     },
     { 
       startTime: 267, 
-      endTime: 271,
-      japanese: "らしさ そんなものを抱えては", 
-      romaji: "rashisa sonna mono wo kakaete wa", 
-      korean: "나다움 그런 걸 안고서는"
-    },
-    { 
-      startTime: 271, 
-      endTime: 277,
-      japanese: "喜び悲しみ 不安期待 絶望絶頂 君と僕のものだよ全部", 
-      romaji: "yorokobi kanashimi fuan kitai zetsubou zecchou kimi to boku no mono da yo zenbu", 
-      korean: "기쁨 슬픔 불안 기대 절망 절정 너와 나의 것이야 전부"
+      endTime: 278,
+      japanese: "らしさ そんなものを抱えては 喜び悲しみ 不安期待 絶望絶頂 君と僕のものだよ全部", 
+      romaji: "rashisa sonna mono wo kakaete wa yorokobi kanashimi fuan kitai zetsubou zecchou kimi to boku no mono da yo zenbu", 
+      korean: "나다움 그런 걸 안고서는 기쁨 슬픔 불안 기대 절망 절정 너와 나의 것이야 전부",
+      koreanPronunciation: "라시사 손나 모노오 카카에테 와 요로코비 카나시미 후안 키타이 제츠보우 젯초우 키미토 보쿠노 모노다요 젠부"
     },
     { 
       startTime: 279, 
-      endTime: 283,
-      japanese: "らしさ そんなものを抱えては", 
-      romaji: "rashisa sonna mono wo kakaete wa", 
-      korean: "나다움 그런 걸 안고서는"
-    },
-    { 
-      startTime: 283, 
-      endTime: 287,
-      japanese: "ああ 息絶えるまで泣くんだ 笑うんだ", 
-      romaji: "aa iki taeru made naku n da warau n da", 
-      korean: "아 숨이 끊어질 때까지 울고 웃는 거야"
-    },
-    { 
-      startTime: 287, 
       endTime: 288,
-      japanese: "本当に良かった ああ 生きてて良かったな", 
-      romaji: "hontou ni yokatta aa ikitete yokatta na", 
-      korean: "정말 다행이다 아 살아 있어서 다행이야",
-      koreanPronunciation: "혼토우니 요카타 아 이키테테 요카타나"
+      japanese: "らしさ そんなものを抱えては ああ 息絶えるまで泣くんだ 笑うんだ 本当に良かった ああ 生きてて良かったな", 
+      romaji: "rashisa sonna mono wo kakaete wa aa iki taeru made naku n da warau n da hontou ni yokatta aa ikitete yokatta na", 
+      korean: "나다움 그런 걸 안고서는 아 숨이 끊어질 때까지 울고 웃는 거야 정말 다행이다 아 살아 있어서 다행이야",
+      koreanPronunciation: "라시사 손나 모노오 카카에테 와 아 이키 타에루 마데 나쿤다 와라운다 혼토우니 요카타 아 이키테테 요카타나"
     }
   ]
 };
